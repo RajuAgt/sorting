cb6f326d4528b7c5964e04da4053ec1fdea5b153
priya <priyankakoppula.rkv@gamil.com>
2019-10-22 00:35:49 +0530

    code change

--- HeapSort.java
+++ HeapSort.java
@@ -17,30 +17,36 @@ public class HeapSort {
 
 	public static void heapSort(int []arr) {
 		
-		int iSize = arr.length,  temp =0;
+		int iSize = arr.length-1,  temp =0;
 		buildMaxHeap(arr, iSize);
-		for(int i=(iSize-1); i >0; i--) {
+		
+		for(int i=iSize; i >0; i--) {
 			
 			//Swap
 			temp = arr[0];
 			arr[0] = arr[i];
 			arr[i] = temp;
 			
-			maxHeapify(arr, 1, iSize--);
+			maxHeapify(arr, 0, --iSize);
 		}
 		
 	}
 	
 	public static void buildMaxHeap(int [] arr, int iSize) {
 		
-		for(int i=(iSize-1)/2; i>=0; i--) {
+		Arrays.stream(arr).forEach(i -> System.out.print(i+" "));
+		System.out.println("");		
+		
+		for(int i=((iSize-1)/2) - 1; i>=0; i--) {
 			maxHeapify(arr, i, iSize-1);
 		}
+		Arrays.stream(arr).forEach(i -> System.out.print(i+" "));
+		System.out.println("");
 	}
 	
 	public static void maxHeapify(int []arr, int i, int iSize) {
 		
-		int iLeft = 2*i, iRight = (2*i)+1, ilarge=0, temp =0;
+		int iLeft = 2*(i+1)-1, iRight = (2*(i+1)), ilarge=0, temp =0;
 		
 		if(iLeft <= iSize && arr[iLeft] > arr[i]) {
 			ilarge = iLeft;
@@ -56,6 +62,8 @@ public class HeapSort {
 			temp = arr[i];
 			arr[i] = arr[ilarge];
 			arr[ilarge] = temp;
+			Arrays.stream(arr).forEach(j -> System.out.print(j+" "));
+			System.out.println("");
 			maxHeapify(arr, ilarge, iSize);
 		}
 	}
